f3f349d3388f1207247a26fdcb4060edc04b6006
yashica52327-ops <yashica52327@gmail.com>
2026-08-08 01:40:18 +0530

    Time: 5 ms (38.75%), Space: 43.1 MB (68.76%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int k = i + j - 1;
+
+                // Take character from s1
+                if (i > 0 && dp[i - 1][j] &&
+                    s1.charAt(i - 1) == s3.charAt(k)) {
+
+                    dp[i][j] = true;
+                }
+
+                // Take character from s2
+                if (j > 0 && dp[i][j - 1] &&
+                    s2.charAt(j - 1) == s3.charAt(k)) {
+
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+}
