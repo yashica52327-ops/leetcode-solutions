3979c5cc38b31c2fd05dc160dc7daedb3ec0242b
yashica52327-ops <yashica52327@gmail.com>
2026-09-23 17:18:21 +0530

    Time: 0 ms (100%), Space: 48 MB (91.14%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
